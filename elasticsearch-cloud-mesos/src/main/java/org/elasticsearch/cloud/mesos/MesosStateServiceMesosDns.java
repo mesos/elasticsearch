a0422b4a2e56294c4fbc7fc811d65e547fa9a3cb
Martin Westergaard Lassen <mwl@trifork.com>
2015-04-01 18:00:34 +0100

    #4 looking up tasks with standard Java instead of dnsjava

--- elasticsearch-cloud-mesos/src/main/java/org/elasticsearch/cloud/mesos/MesosStateServiceMesosDns.java
+++ elasticsearch-cloud-mesos/src/main/java/org/elasticsearch/cloud/mesos/MesosStateServiceMesosDns.java
@@ -6,69 +6,79 @@ import org.elasticsearch.common.component.AbstractLifecycleComponent;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.discovery.mesos.MesosUnicastHostsProvider;
-import org.xbill.DNS.*;
 
-import java.net.UnknownHostException;
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import javax.naming.directory.Attributes;
+import javax.naming.directory.DirContext;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MesosStateServiceMesosDns extends AbstractLifecycleComponent<MesosStateService> implements MesosStateService {
     private final String dnsServer;
+    private final DirContext ctx;
 
     @Inject
-    public MesosStateServiceMesosDns(Settings settings) {
+    public MesosStateServiceMesosDns(Settings settings, DirContext ctx) {
         super(settings);
         dnsServer = settings.get("cloud.mesos.resolver", "localhost");
+        this.ctx = ctx;
     }
 
     @Override
     protected void doStart() throws ElasticsearchException {
-
     }
 
     @Override
     protected void doStop() throws ElasticsearchException {
-
     }
 
     @Override
     protected void doClose() throws ElasticsearchException {
-
     }
 
     @Override
     public List<String> getNodeIpsAndPorts(MesosUnicastHostsProvider mesosUnicastHostsProvider) {
-        logger.debug("Using resolver: {}", dnsServer);
+        logger.warn("Using resolver: {}", dnsServer);
+        //TODO: (MWL) remove hardcoded framework name
+        String taskHostName = "_elasticsearch-mesos._tcp.marathon.mesos";
         final ArrayList<String> nodes = Lists.newArrayList();
+
+        Attributes attrs;
+        NamingEnumeration<?> enumeration;
         try {
-            //TODO: (MWL) remove hardcoded framework name
-            final Lookup lookup = new Lookup("_elasticsearch-mesos._tcp.marathon.mesos", Type.SRV);
-            lookup.setResolver(new SimpleResolver(dnsServer));
-            final Record[] records = lookup.run();
+            attrs = ctx.getAttributes(taskHostName, new String[]{"SRV"});
+            enumeration = attrs.get("SRV").getAll();
+            while (enumeration.hasMore()) {
+                SrvRecord record = new SrvRecord((String) enumeration.next());
 
-            if (lookup.getResult() != Lookup.SUCCESSFUL) {
-                throw new RuntimeException("Failed to resolver task: " + lookup.getErrorString());
+                nodes.add(record.getHostAndPort());
             }
-
-            for (Record record : records) {
-                if (record instanceof SRVRecord) {
-                    final SRVRecord srvRecord = (SRVRecord) record;
-                    //TODO: (MWL) first entry of what?
-                    final String node = srvRecord.getTarget().getLabelString(0) + ":" + srvRecord.getPort();
-                    logger.debug("Adding node {}", node);
-                    nodes.add(node);
-                }
-                else {
-                    logger.warn("Ignoring record: {}", record);
-                }
-            }
-
-        } catch (TextParseException e) {
-            throw new RuntimeException("Could not perform dns lookup", e);
-        } catch (UnknownHostException e) {
-            throw new RuntimeException("Could not find resolver " + dnsServer, e);
+        } catch (NamingException e) {
+            throw new RuntimeException("Failed to resolve hostname", e);
         }
 
         return nodes;
     }
+
+    private static class SrvRecord {
+        private final String[] fields;
+
+        public SrvRecord(String record) {
+            fields = record.split(" ");
+        }
+
+        public String getPort() {
+            return fields[2];
+        }
+
+        public String getHost() {
+            return fields[3];
+        }
+
+        public String getHostAndPort() {
+            return getHost() + ":" + getPort();
+        }
+    }
+
 }
