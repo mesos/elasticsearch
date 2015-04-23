938f0d64f675ee46bcaafe70b61d3099b057c2ce
Frank Scholten <frank@frankscholten.nl>
2015-04-23 18:21:00 +0200

    Refactored launchTasks method to prevent TASK_LOST status.

--- scheduler/deploy-to-marathon.sh
+++ scheduler/deploy-to-marathon.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-curl -sk -XPOST -d @marathon.json -H "Content-Type: application/json" http://master:8080/v2/apps
+curl -k -XPOST -d @marathon.json -H "Content-Type: application/json" http://master:8080/v2/apps
