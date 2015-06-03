6bfc57f400e3a619cf8ef64e762bbd2fdd5b37ac
Frank Scholten <frank@frankscholten.nl>
2015-06-03 23:46:48 +0200

    Transport port is now parsed from state file.

--- elasticsearch-cloud-mesos/src/main/java/org/elasticsearch/cloud/mesos/MesosStateServiceRest.java
+++ elasticsearch-cloud-mesos/src/main/java/org/elasticsearch/cloud/mesos/MesosStateServiceRest.java
@@ -1,9 +1,5 @@
 package org.elasticsearch.cloud.mesos;
 
-import com.google.protobuf.InvalidProtocolBufferException;
-import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.exceptions.UnirestException;
-import org.apache.mesos.Protos;
 import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.common.collect.Lists;
 import org.elasticsearch.common.collect.Maps;
@@ -16,21 +12,29 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Service that manages the lifecycle of Mesos Tasks.
  */
 public class MesosStateServiceRest extends AbstractLifecycleComponent<MesosStateServiceRest> implements MesosStateService {
 
+    // An elasticsearch task has 2 ports, client port and transport port.
+    public static final int TRANSPORT_PORT_INDEX = 1;
+
     private final String master;
+
+    private MesosStateRepository repository;
 
     @Inject
     public MesosStateServiceRest(Settings settings) {
         super(settings);
         master = settings.get("cloud.mesos.master");
+        repository = new MesosStateRepository(master);
     }
 
     @Override
@@ -48,61 +52,70 @@ public class MesosStateServiceRest extends AbstractLifecycleComponent<MesosState
 
     }
 
+    @SuppressWarnings("PMD.CyclomaticComplexity")
     @Override
     public List<Pair<String, Integer>> getNodeIpsAndPorts(MesosUnicastHostsProvider mesosUnicastHostsProvider) {
         logger.debug("Using mesos master {}", master);
 
         List<Pair<String, Integer>> nodeIps = Lists.newArrayList();
+        JSONObject state = repository.retrieveState();
         try {
-            JSONObject state = Unirest.get(master + "/master/state.json").asJson().getBody().getObject();
+            String clusterName = state.getString("cluster");
+            logger.info("received state for cluster name={}", clusterName);
+        } catch (JSONException e) {
+            logger.warn("Mesos 'cluster' property not set");
+        }
 
-            try {
-                String clusterName = state.getString("cluster");
-                logger.info("received state for cluster name={}", clusterName);
-            } catch (JSONException e) {
-                logger.warn("Mesos 'cluster' property not set");
-            }
+        JSONArray slaves = state.getJSONArray("slaves");
+        Map<String, JSONObject> slaveIdMap = Maps.newHashMap();
+        for (int i = 0; i < slaves.length(); i++) {
+            JSONObject slave = slaves.getJSONObject(i);
+            slaveIdMap.put(slave.getString("id"), slave);
+        }
+        logger.info("Found slaves " + slaveIdMap);
 
-            JSONArray slaves = state.getJSONArray("slaves");
-            Map<String, JSONObject> slaveIdMap = Maps.newHashMap();
-            for (int i = 0; i < slaves.length(); i++) {
-                JSONObject slave = slaves.getJSONObject(i);
-                slaveIdMap.put(slave.getString("id"), slave);
-            }
-            logger.info("Found slaves " + slaveIdMap);
+        JSONArray frameworks = state.getJSONArray("frameworks");
+        for (int i = 0; i < frameworks.length(); i++) {
+            JSONObject framework = frameworks.getJSONObject(i);
 
-            JSONArray frameworks = state.getJSONArray("frameworks");
-            for (int i = 0; i < frameworks.length(); i++) {
-                JSONObject framework = frameworks.getJSONObject(i);
+            if (framework.getBoolean("active") && framework.getString("name").equalsIgnoreCase("elasticsearch")) {
+                JSONArray tasks = framework.getJSONArray("tasks");
 
-                if (framework.getBoolean("active") && framework.getString("name").equalsIgnoreCase("elasticsearch")) {
-                    JSONArray tasks = framework.getJSONArray("tasks");
+                for (int j = 0; j < tasks.length(); j++) {
+                    JSONObject task = tasks.getJSONObject(j);
+                    String hostname = slaveIdMap.get(task.getString("slave_id")).getString("hostname");
+                    logger.info("Found slave hostname " + hostname);
+                    JSONObject resources = task.getJSONObject("resources");
 
-                    for (int j = 0; j < tasks.length(); j++) {
-                        JSONObject task = tasks.getJSONObject(j);
-                        String hostname = slaveIdMap.get(task.getString("slave_id")).getString("hostname");
-                        logger.info("Found slave hostname " + hostname);
-                        JSONObject resources = task.getJSONObject("resources");
-                        long transportPort;
-                        try {
-                            String ports = resources.getString("ports");
-                            Protos.Resource resource = Protos.Resource.PARSER.parseFrom(ports.getBytes(StandardCharsets.UTF_8));
-                            Protos.Value.Range range = resource.getRanges().getRange(0);
-                            transportPort = range.getEnd();
-                        } catch (InvalidProtocolBufferException e) {
-                            logger.error("Could not parse port range from task " + task.getString("id"));
-                            return nodeIps;
-                        }
+                    String portRanges = resources.getString("ports");
+                    logger.info("Ports " + portRanges);
 
-                        Pair<String, Integer> ipAndPort = Pair.of(hostname, (int) transportPort);
-                        nodeIps.add(ipAndPort);
+                    Pattern pattern = Pattern.compile("\\[(\\d+)\\-(\\d+)(, (\\d+)\\-(\\d+))*\\]");
+                    Matcher matcher = pattern.matcher(portRanges);
+                    if (!matcher.matches()) {
+                        throw new RuntimeException("Could not parse port ranges: " + portRanges);
                     }
+                    logger.info("Found " + matcher.groupCount() + " groups");
+
+                    List<Integer> portNumbers = new ArrayList<>();
+                    int numberOfRanges = (matcher.groupCount() - 1) / 2;
+                    for (int k = 0; k < numberOfRanges; k += 1) {
+                        int beginPort = Integer.parseInt(matcher.group(3 * k + 1));
+                        int endPort = Integer.parseInt(matcher.group(3 * k + 2));
+                        for (int l = beginPort; l <= endPort; l++) {
+                             portNumbers.add(l);
+                        }
+                    }
+
+                    Pair<String, Integer> ipAndPort = Pair.of(hostname, portNumbers.get(TRANSPORT_PORT_INDEX));
+                    nodeIps.add(ipAndPort);
                 }
             }
-
-        } catch (UnirestException e) {
-            logger.warn("Failed to fetch cluster state", e);
         }
         return nodeIps;
+    }
+
+    public void setRepository(MesosStateRepository repository) {
+        this.repository = repository;
     }
 }
---
+++ elasticsearch-cloud-mesos/src/test/java/org/elasticsearch/cloud/mesos/MesosStateServiceRestTest.java
@@ -0,0 +1,49 @@
+package org.elasticsearch.cloud.mesos;
+
+import org.apache.commons.io.IOUtils;
+import org.elasticsearch.Version;
+import org.elasticsearch.common.lang3.tuple.Pair;
+import org.elasticsearch.common.settings.ImmutableSettings;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.discovery.mesos.MesosUnicastHostsProvider;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Tests parsing of slave host and port numbers Mesos state file.
+ */
+public class MesosStateServiceRestTest {
+
+    @Test
+    public void testGetNodeIpsAndPorts() throws IOException {
+        Settings settings = ImmutableSettings.builder().put("cloud.mesos.master", "master").build();
+
+        MesosStateServiceRest mesosStateServiceRest = new MesosStateServiceRest(settings);
+
+        MesosStateRepository repository = mock(MesosStateRepository.class);
+        JSONObject jsonObject = new JSONObject(IOUtils.toString(new FileInputStream("src/test/resources/state.json")));
+        when(repository.retrieveState()).thenReturn(jsonObject);
+        mesosStateServiceRest.setRepository(repository);
+
+        List<Pair<String, Integer>> nodeIpsAndPorts = mesosStateServiceRest.getNodeIpsAndPorts(new MesosUnicastHostsProvider(settings, mesosStateServiceRest, Version.V_1_4_0));
+
+        assertThat(nodeIpsAndPorts.get(0).getKey(), is("slave1"));
+        assertThat(nodeIpsAndPorts.get(0).getValue(), is(9300));
+
+        assertThat(nodeIpsAndPorts.get(1).getKey(), is("slave2"));
+        assertThat(nodeIpsAndPorts.get(1).getValue(), is(9300));
+
+        assertThat(nodeIpsAndPorts.get(2).getKey(), is("slave3"));
+        assertThat(nodeIpsAndPorts.get(2).getValue(), is(9300));
+    }
+
+}
