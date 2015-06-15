749b02d253073e9ab7e1fe2421beb9079f8b29df
Frank Scholten <frank@frankscholten.nl>
2015-06-15 15:03:27 -0700

    Made system test more robust

--- system-test/src/test/java/org/apache/mesos/elasticsearch/systemtest/DiscoverySystemTest.java
+++ system-test/src/test/java/org/apache/mesos/elasticsearch/systemtest/DiscoverySystemTest.java
@@ -7,17 +7,13 @@ import com.github.dockerjava.core.DockerClientConfig;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import org.apache.log4j.Logger;
-import org.apache.mesos.elasticsearch.common.Discovery;
-import org.json.JSONObject;
 import org.junit.Test;
 
 import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
 
 import static com.jayway.awaitility.Awaitility.await;
-import static org.hamcrest.core.IsNull.notNullValue;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.hamcrest.CoreMatchers.is;
 
 /**
  * Tests REST node discovery
@@ -28,6 +24,20 @@ public class DiscoverySystemTest {
 
     @Test
     public void testNodeDiscoveryRest() throws InterruptedException {
+        String slave1Ip = getSlaveIp("mesoses_slave1_1");
+        String slave2Ip = getSlaveIp("mesoses_slave2_1");
+        String slave3Ip = getSlaveIp("mesoses_slave3_1");
+
+        ElasticsearchNodesResponse nodesResponse = new ElasticsearchNodesResponse(slave1Ip, slave2Ip, slave3Ip);
+
+        assertNodesDiscovered(nodesResponse);
+    }
+
+    private void assertNodesDiscovered(ElasticsearchNodesResponse nodesResponse) {
+        await().atMost(60, TimeUnit.SECONDS).pollInterval(1, TimeUnit.SECONDS).until(nodesResponse, is(true));
+    }
+
+    private String getSlaveIp(String slaveName) {
         DockerClientConfig config = DockerClientConfig.createDefaultConfigBuilder()
                 .withVersion("1.16")
                 .withUri("unix:///var/run/docker.sock")
@@ -35,32 +45,37 @@ public class DiscoverySystemTest {
 
         DockerClient docker = DockerClientBuilder.getInstance(config).build();
 
-        InspectContainerResponse response = docker.inspectContainerCmd("mesoses_slave1_1").exec();
+        InspectContainerResponse response = docker.inspectContainerCmd(slaveName).exec();
 
-        String ipAddress = response.getNetworkSettings().getIpAddress();
-
-        ElasticsearchNodesResponse nodesResponse = new ElasticsearchNodesResponse(ipAddress);
-        JSONObject jsonNodesResponse = await().atMost(60, TimeUnit.SECONDS).pollInterval(1, TimeUnit.SECONDS).until(nodesResponse, notNullValue());
-
-        assertEquals(3, jsonNodesResponse.getJSONObject("nodes").length());
+        return response.getNetworkSettings().getIpAddress();
     }
 
-    private static class ElasticsearchNodesResponse implements Callable<JSONObject> {
+    private static class ElasticsearchNodesResponse implements Callable<Boolean> {
 
-        private String ipAddress;
+        private String ipAddress1;
+        private String ipAddress2;
+        private String ipAddress3;
 
-        public ElasticsearchNodesResponse(String ipAddress) {
-            this.ipAddress = ipAddress;
+        public ElasticsearchNodesResponse(String ipAddress1, String ipAddress2, String ipAddress3) {
+            this.ipAddress1 = ipAddress1;
+            this.ipAddress2 = ipAddress2;
+            this.ipAddress3 = ipAddress3;
         }
 
         @Override
-        public JSONObject call() throws Exception {
+        public Boolean call() throws Exception {
             try {
-                return Unirest.get("http://" + ipAddress + ":9200/_nodes").asJson().getBody().getObject();
+                double nodesSlave1 = Unirest.get("http://" + ipAddress1 + ":9200/_nodes").asJson().getBody().getObject().getJSONObject("nodes").length();
+                double nodesSlave2 = Unirest.get("http://" + ipAddress2 + ":9200/_nodes").asJson().getBody().getObject().getJSONObject("nodes").length();
+                double nodesSlave3 = Unirest.get("http://" + ipAddress3 + ":9200/_nodes").asJson().getBody().getObject().getJSONObject("nodes").length();
+                if (!(nodesSlave1 == 3 && nodesSlave2 == 3 && nodesSlave3 == 3)) {
+                    return false;
+                }
             } catch (UnirestException e) {
                 LOGGER.info("Elasticsearch does not yet listen on port 9200");
-                return null;
+                return false;
             }
+            return true;
         }
     }
 
