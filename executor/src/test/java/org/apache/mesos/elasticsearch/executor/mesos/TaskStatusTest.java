eb02d6864df854658a33a591e4b3ad84b37f2687
Phil Winder <pnw@trifork.com>
2015-06-23 11:02:48 +0100

    TaskStatus test.

---
+++ executor/src/test/java/org/apache/mesos/elasticsearch/executor/mesos/TaskStatusTest.java
@@ -0,0 +1,26 @@
+package org.apache.mesos.elasticsearch.executor.mesos;
+
+import org.apache.mesos.Protos;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Tests
+ */
+public class TaskStatusTest {
+    @Test(expected = NullPointerException.class)
+    public void shouldExceptionIfPassedNull() {
+        new TaskStatus(null);
+    }
+
+    @Test
+    public void shouldReturnValidProtos() {
+        TaskStatus status = new TaskStatus(Protos.TaskID.getDefaultInstance());
+        assertNotNull(status.starting());
+        assertNotNull(status.running());
+        assertNotNull(status.finished());
+        assertNotNull(status.failed());
+        assertNotNull(status.error());
+    }
+}
