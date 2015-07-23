9c12523c9730e486ce4cbe3d91a70f8c518a21e5
Phil Winder <pnw@trifork.com>
2015-07-23 15:24:15 +0100

    Added docker host tests.

--- system-test/src/systemTest/java/org/apache/mesos/elasticsearch/systemtest/TestBase.java
+++ system-test/src/systemTest/java/org/apache/mesos/elasticsearch/systemtest/TestBase.java
@@ -50,6 +50,7 @@ public abstract class TestBase {
 
         CreateContainerCmd createCommand = docker
                 .createContainerCmd(schedulerImage)
+                .withNetworkMode("host")
                 .withExtraHosts(IntStream.rangeClosed(1, config.numberOfSlaves).mapToObj(value -> "slave" + value + ":" + ipAddress).toArray(String[]::new))
                 .withCmd("-zk", "zk://" + ipAddress + ":2181/mesos", "-n", "3", "-ram", "64");
 
