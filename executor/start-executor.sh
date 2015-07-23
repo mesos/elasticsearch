67669c5cbf75fc0d26fe095bb191af7b29994dfb
Phil Winder <pnw@trifork.com>
2015-07-23 14:07:02 +0100

    Bug, needed to pass command line arguments through shell script.

--- executor/start-executor.sh
+++ executor/start-executor.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java $JAVA_OPTS -Djava.library.path=/usr/local/lib -jar /tmp/elasticsearch-mesos-executor.jar
+java $JAVA_OPTS -Djava.library.path=/usr/local/lib -jar /tmp/elasticsearch-mesos-executor.jar $@
