a671e71e0c4933d586238fd50a3fc738506a82d6
Phil Winder <pnw@trifork.com>
2015-07-24 12:31:39 +0100

    Added mesos ZK formatter so no matter what users enter into the ZK field, it will be parsed in the format correct for mesos. Deleted ZKAddressFormatter because it wasn't used.

--- commons/src/main/java/org/apache/mesos/elasticsearch/common/zookeeper/formatter/MesosZKFormatter.java
+++ commons/src/main/java/org/apache/mesos/elasticsearch/common/zookeeper/formatter/MesosZKFormatter.java
@@ -9,27 +9,30 @@ import java.util.List;
 /**
  * Provides the ZooKeeper address(es) in a format required by Mesos.
  *
- * The format consists of a list of ZK servers
+ * The format consists of a list of ZK servers and appends a /mesos
  *
- * Example: zk://host1:port1,host2:port2
+ * Example: zk://host1:port1,host2:port2/mesos
  */
-public class MesosStateZKFormatter extends AbstractZKFormatter {
+public class MesosZKFormatter extends AbstractZKFormatter {
 
-    public MesosStateZKFormatter(ZKAddressParser parser) {
+    public static final String MESOS_PATH = "/mesos";
+
+    public MesosZKFormatter(ZKAddressParser parser) {
         super(parser);
     }
 
     /**
-     * Get the ZooKeeper address, correctly formatted.
+     * Get the ZooKeeper address for the mesos master, correctly formatted.
      * @param zkUrl The raw ZK address string in the format "zk://host:port/zkNode,..."
      * @throws ZKAddressException if the raw zkURL is invalid.
-     * @return the zookeeper addresses in the format host:port[,host:port,...]
+     * @return the zookeeper addresses in the format host:port[,host:port,...]/mesos
      */
     public String format(String zkUrl) {
         List<ZKAddress> addressList = parser.validateZkUrl(zkUrl);
         StringBuilder builder = new StringBuilder();
         addressList.forEach(add -> builder.append(",").append(add.getAddress()).append(":").append(add.getPort()));
         builder.deleteCharAt(0); // Delete first ','
+        builder.append(MESOS_PATH);
         return builder.toString();
     }
 
--- scheduler/src/main/java/org/apache/mesos/elasticsearch/scheduler/Main.java
+++ scheduler/src/main/java/org/apache/mesos/elasticsearch/scheduler/Main.java
@@ -2,6 +2,7 @@ package org.apache.mesos.elasticsearch.scheduler;
 
 import org.apache.commons.cli.*;
 import org.apache.mesos.elasticsearch.common.zookeeper.formatter.MesosStateZKFormatter;
+import org.apache.mesos.elasticsearch.common.zookeeper.formatter.MesosZKFormatter;
 import org.apache.mesos.elasticsearch.common.zookeeper.formatter.ZKFormatter;
 import org.apache.mesos.elasticsearch.common.zookeeper.parser.ZKAddressParser;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -76,15 +77,22 @@ public class Main {
             return;
         }
 
-        ZKFormatter zkFormatter = new MesosStateZKFormatter(new ZKAddressParser());
-        String mesosStateZkUrl = zkFormatter.format(zkUrl);
-
-        configuration.setZookeeperUrl(zkUrl);
+        configuration.setZookeeperUrl(getMesosZKURL(zkUrl));
         configuration.setVersion(getClass().getPackage().getImplementationVersion());
         configuration.setNumberOfHwNodes(Integer.parseInt(numberOfHwNodesString));
-        configuration.setState(new State(new ZooKeeperStateInterfaceImpl(mesosStateZkUrl)));
+        configuration.setState(new State(new ZooKeeperStateInterfaceImpl(getMesosStateZKURL(zkUrl))));
         configuration.setMem(Double.parseDouble(ram));
         configuration.setManagementApiPort(Integer.parseInt(managementApiPort));
+    }
+
+    private String getMesosStateZKURL(String zkUrl) {
+        ZKFormatter mesosStateZKFormatter = new MesosStateZKFormatter(new ZKAddressParser());
+        return mesosStateZKFormatter.format(zkUrl);
+    }
+
+    private String getMesosZKURL(String zkUrl) {
+        ZKFormatter mesosZKFormatter = new MesosZKFormatter(new ZKAddressParser());
+        return mesosZKFormatter.format(zkUrl);
     }
 
     private void printUsageAndExit() {
