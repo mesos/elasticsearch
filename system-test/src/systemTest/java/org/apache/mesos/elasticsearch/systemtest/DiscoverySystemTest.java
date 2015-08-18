d2560ea5b69dfaaef98977503807fbec78003299
James Fisher <jhf@trifork.com>
2015-08-18 09:51:59 +0100

    test that Mesos reports two frameworks after registering two

--- system-test/src/systemTest/java/org/apache/mesos/elasticsearch/systemtest/DiscoverySystemTest.java
+++ system-test/src/systemTest/java/org/apache/mesos/elasticsearch/systemtest/DiscoverySystemTest.java
@@ -1,11 +1,18 @@
 package org.apache.mesos.elasticsearch.systemtest;
 
 import org.apache.log4j.Logger;
+import org.apache.mesos.mini.state.Framework;
 import org.json.JSONObject;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
+import static com.jayway.awaitility.Awaitility.await;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -16,6 +23,18 @@ public class DiscoverySystemTest extends TestBase {
     public static final Logger LOGGER = Logger.getLogger(DiscoverySystemTest.class);
 
     @Test
+    public void testSchedulerRegistration() {
+        await().atMost(60, TimeUnit.SECONDS).until(new Callable<Boolean>() {
+            @Override
+            public Boolean call() throws Exception {
+                ArrayList<Framework> frameworks = CLUSTER.getStateInfo().getFrameworks();
+                Set<String> registeredFrameworkNames = frameworks.stream().map((Framework f) -> f.getName()).collect(Collectors.toSet());
+                LOGGER.info("Waiting for one framework in cluster; got: " + registeredFrameworkNames.toString());
+                return registeredFrameworkNames.size() == 1;
+            }
+        });
+    }
+
     public void testNodeDiscoveryRest() {
         ElasticsearchSchedulerContainer scheduler = getScheduler();
 
