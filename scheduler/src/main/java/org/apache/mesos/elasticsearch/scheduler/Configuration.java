65ef81cbddc91edc2bfa3699be4edf1e45bf6acd
Phil Winder <pnw@trifork.com>
2015-08-25 10:55:46 +0100

    Refactored Zookeeper CLI options into common, so executor can use. Moved validators.

--- scheduler/src/main/java/org/apache/mesos/elasticsearch/scheduler/Configuration.java
+++ scheduler/src/main/java/org/apache/mesos/elasticsearch/scheduler/Configuration.java
@@ -1,8 +1,12 @@
 package org.apache.mesos.elasticsearch.scheduler;
 
-import com.beust.jcommander.*;
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import org.apache.log4j.Logger;
 import org.apache.mesos.Protos;
+import org.apache.mesos.elasticsearch.common.cli.validators.CLIValidators;
+import org.apache.mesos.elasticsearch.common.zookeeper.ZookeeperCLIParameter;
 import org.apache.mesos.elasticsearch.common.zookeeper.formatter.MesosStateZKFormatter;
 import org.apache.mesos.elasticsearch.common.zookeeper.formatter.MesosZKFormatter;
 import org.apache.mesos.elasticsearch.common.zookeeper.formatter.ZKFormatter;
@@ -20,9 +24,13 @@ import java.util.concurrent.TimeUnit;
 @SuppressWarnings("PMD.TooManyFields")
 public class Configuration {
     private static final Logger LOGGER = Logger.getLogger(Configuration.class);
+    // **** ZOOKEEPER
+    private final ZookeeperCLIParameter zookeeperCLI = new ZookeeperCLIParameter();
 
     public Configuration(String[] args) {
-        final JCommander jCommander = new JCommander(this);
+        final JCommander jCommander = new JCommander();
+        jCommander.addObject(zookeeperCLI);
+        jCommander.addObject(this);
         try {
             jCommander.parse(args); // Parse command line args into configuration class.
         } catch (com.beust.jcommander.ParameterException ex) {
@@ -33,39 +41,24 @@ public class Configuration {
         }
     }
 
-    // **** ZOOKEEPER
-    public static final String ZOOKEEPER_TIMEOUT = "--zookeeperTimeout";
-    @Parameter(names = {ZOOKEEPER_TIMEOUT}, description = "The timeout for connecting to zookeeper (ms).", validateValueWith = PositiveLong.class)
-    private long zookeeperTimeout = 20000L;
-    public long getZookeeperTimeout() {
-        return zookeeperTimeout;
-    }
-
-    public static final String ZOOKEEPER_URL = "--zookeeperUrl";
-    @Parameter(names = {ZOOKEEPER_URL}, required = true, description = "Zookeeper urls in the format zk://IP:PORT,IP:PORT,...)", validateWith = NotEmptyString.class)
-    private String zookeeperUrl = "zk://mesos.master:2181";
-    private String getZookeeperUrl() {
-        return zookeeperUrl;
-    }
-
 
     // **** ELASTICSEARCH
     public static final String ELASTICSEARCH_CPU = "--elasticsearchCpu";
-    @Parameter(names = {ELASTICSEARCH_CPU}, description = "The amount of CPU resource to allocate to the elasticsearch instance.", validateValueWith = PositiveDouble.class)
+    @Parameter(names = {ELASTICSEARCH_CPU}, description = "The amount of CPU resource to allocate to the elasticsearch instance.", validateValueWith = CLIValidators.PositiveDouble.class)
     private double cpus = 1.0;
     public double getCpus() {
         return cpus;
     }
 
     public static final String ELASTICSEARCH_RAM = "--elasticsearchRam";
-    @Parameter(names = {ELASTICSEARCH_RAM}, description = "The amount of ram resource to allocate to the elasticsearch instance (MB).", validateValueWith = PositiveDouble.class)
+    @Parameter(names = {ELASTICSEARCH_RAM}, description = "The amount of ram resource to allocate to the elasticsearch instance (MB).", validateValueWith = CLIValidators.PositiveDouble.class)
     private double mem = 256;
     public double getMem() {
         return mem;
     }
 
     public static final String ELASTICSEARCH_DISK = "--elasticsearchDisk";
-    @Parameter(names = {ELASTICSEARCH_DISK}, description = "The amount of Disk resource to allocate to the elasticsearch instance (MB).", validateValueWith = PositiveDouble.class)
+    @Parameter(names = {ELASTICSEARCH_DISK}, description = "The amount of Disk resource to allocate to the elasticsearch instance (MB).", validateValueWith = CLIValidators.PositiveDouble.class)
     private double disk = 1024;
     public double getDisk() {
         return disk;
@@ -79,7 +72,7 @@ public class Configuration {
     }
 
     public static final String ELASTICSEARCH_CLUSTER_NAME = "--elasticsearchClusterName";
-    @Parameter(names = {ELASTICSEARCH_CLUSTER_NAME}, description = "Name of the elasticsearch cluster", validateWith = NotEmptyString.class)
+    @Parameter(names = {ELASTICSEARCH_CLUSTER_NAME}, description = "Name of the elasticsearch cluster", validateWith = CLIValidators.NotEmptyString.class)
     private String elasticsearchClusterName = "mesos-ha";
     public String getElasticsearchClusterName() {
         return elasticsearchClusterName;
@@ -87,7 +80,7 @@ public class Configuration {
 
     // **** WEB UI
     public static final String WEB_UI_PORT = "--webUiPort";
-    @Parameter(names = {WEB_UI_PORT}, description = "TCP port for web ui interface.", validateValueWith = PositiveInteger.class)
+    @Parameter(names = {WEB_UI_PORT}, description = "TCP port for web ui interface.", validateValueWith = CLIValidators.PositiveInteger.class)
     private int webUiPort = 31100; // Default is more likely to work on a default Mesos installation
     public int getWebUiPort() {
         return webUiPort;
@@ -101,14 +94,14 @@ public class Configuration {
     }
 
     public static final String FRAMEWORK_NAME = "--frameworkName";
-    @Parameter(names = {FRAMEWORK_NAME}, description = "The name given to the framework.", validateWith = NotEmptyString.class)
+    @Parameter(names = {FRAMEWORK_NAME}, description = "The name given to the framework.", validateWith = CLIValidators.NotEmptyString.class)
     private String frameworkName = "elasticsearch";
     public String getFrameworkName() {
         return frameworkName;
     }
 
     public static final String EXECUTOR_NAME = "--executorName";
-    @Parameter(names = {EXECUTOR_NAME}, description = "The name given to the executor task.", validateWith = NotEmptyString.class)
+    @Parameter(names = {EXECUTOR_NAME}, description = "The name given to the executor task.", validateWith = CLIValidators.NotEmptyString.class)
     private String executorName = "elasticsearch-executor";
     public String getTaskName() {
         return executorName;
@@ -116,14 +109,14 @@ public class Configuration {
 
     // DCOS Certification requirement 01
     public static final String FRAMEWORK_FAILOVER_TIMEOUT = "--frameworkFailoverTimeout";
-    @Parameter(names = {FRAMEWORK_FAILOVER_TIMEOUT}, description = "The time before Mesos kills a scheduler and tasks if it has not recovered (ms).", validateValueWith = PositiveDouble.class)
+    @Parameter(names = {FRAMEWORK_FAILOVER_TIMEOUT}, description = "The time before Mesos kills a scheduler and tasks if it has not recovered (ms).", validateValueWith = CLIValidators.PositiveDouble.class)
     private double frameworkFailoverTimeout = 2592000; // Mesos will kill framework after 1 month if marathon does not restart.
     public double getFailoverTimeout() {
         return frameworkFailoverTimeout;
     }
 
     public static final String EXECUTOR_HEALTH_DELAY = "--executorHealthDelay";
-    @Parameter(names = {EXECUTOR_HEALTH_DELAY}, description = "The delay between executor healthcheck requests (ms).", validateValueWith = PositiveLong.class)
+    @Parameter(names = {EXECUTOR_HEALTH_DELAY}, description = "The delay between executor healthcheck requests (ms).", validateValueWith = CLIValidators.PositiveLong.class)
     private static Long executorHealthDelay = 30000L;
     public Long getExecutorHealthDelay() {
         return executorHealthDelay;
@@ -140,7 +133,7 @@ public class Configuration {
 
     public static final String EXECUTOR_IMAGE = "--executorImage";
     public static final String DEFAULT_EXECUTOR_IMAGE = "mesos/elasticsearch-executor";
-    @Parameter(names = {EXECUTOR_IMAGE}, description = "The docker executor image to use.", validateWith = NotEmptyString.class)
+    @Parameter(names = {EXECUTOR_IMAGE}, description = "The docker executor image to use.", validateWith = CLIValidators.NotEmptyString.class)
     private String executorImage = DEFAULT_EXECUTOR_IMAGE;
     public String getEexecutorImage() {
         return executorImage;
@@ -172,7 +165,7 @@ public class Configuration {
         if (state == null) {
             org.apache.mesos.state.State zkState = new ZooKeeperState(
                     getMesosStateZKURL(),
-                    getZookeeperTimeout(),
+                    zookeeperCLI.getZookeeperTimeout(),
                     TimeUnit.MILLISECONDS,
                     "/" + getFrameworkName() + "/" + getElasticsearchClusterName());
             state = new SerializableZookeeperState(zkState);
@@ -182,76 +175,18 @@ public class Configuration {
 
     public String getMesosStateZKURL() {
         ZKFormatter mesosStateZKFormatter = new MesosStateZKFormatter(new ZKAddressParser());
-        return mesosStateZKFormatter.format(getZookeeperUrl());
+        return mesosStateZKFormatter.format(zookeeperCLI.getZookeeperUrl());
     }
 
     public String getMesosZKURL() {
         ZKFormatter mesosZKFormatter = new MesosZKFormatter(new ZKAddressParser());
-        return mesosZKFormatter.format(getZookeeperUrl());
-    }
-
-    /**
-     * Abstract class to validate a number.
-     * @param <T> A numeric type
-     */
-    public abstract static class PositiveValue<T> implements IValueValidator<T> {
-        @Override
-        public void validate(String name, T value) throws ParameterException {
-            if (notValid(value)) {
-                throw new ParameterException("Parameter " + name + " should be greater than zero (found " + value + ")");
-            }
-        }
-
-        public abstract Boolean notValid(T value);
-    }
-
-    /**
-     * Validates a positive number. For type Long
-     */
-    public static class PositiveLong extends PositiveValue<Long> {
-        @Override
-        public Boolean notValid(Long value) {
-            return value <= 0;
-        }
-    }
-
-    /**
-     * Validates a positive number. For type Double
-     */
-    public static class PositiveDouble extends PositiveValue<Double> {
-        @Override
-        public Boolean notValid(Double value) {
-            return value <= 0;
-        }
-    }
-
-    /**
-     * Validates a positive number. For type Integer
-     */
-    public static class PositiveInteger extends PositiveValue<Integer> {
-        @Override
-        public Boolean notValid(Integer value) {
-            return value <= 0;
-        }
-    }
-
-    /**
-     * Adds a warning message if an even number is encountered
-     */
-    public static class OddNumberOfNodes extends PositiveInteger {
-        @Override
-        public Boolean notValid(Integer value) {
-            if (value % 2 == 0) {
-                LOGGER.warn("Setting number of ES nodes to an even number. Not recommended!");
-            }
-            return super.notValid(value);
-        }
+        return mesosZKFormatter.format(zookeeperCLI.getZookeeperUrl());
     }
 
     /**
      * Ensures that the number is > than the EXECUTOR_HEALTH_DELAY
      */
-    public static class GreaterThanHealthDelay extends PositiveLong {
+    public static class GreaterThanHealthDelay extends CLIValidators.PositiveLong {
         @Override
         public void validate(String name, Long value) throws ParameterException {
             if (notValid(value) || value <= Configuration.executorHealthDelay) {
@@ -261,14 +196,15 @@ public class Configuration {
     }
 
     /**
-     * Ensures that the string is not empty. Will strip spaces.
+     * Adds a warning message if an even number is encountered
      */
-    public static class NotEmptyString implements IParameterValidator {
+    public static class OddNumberOfNodes extends CLIValidators.PositiveInteger {
         @Override
-        public void validate(String name, String value) throws ParameterException {
-            if (value.replace(" ", "").isEmpty()) {
-                throw new ParameterException("Parameter " + name + " cannot be empty");
+        public Boolean notValid(Integer value) {
+            if (value % 2 == 0) {
+                LOGGER.warn("Setting number of ES nodes to an even number. Not recommended!");
             }
+            return super.notValid(value);
         }
     }
 }
