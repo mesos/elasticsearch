6e2a9f812821e90ea35870b56b762ead66c4b426
James Fisher <jhf@trifork.com>
2015-09-08 11:24:49 +0200

    mock framework role in FrameworkInfoFactoryTest

--- scheduler/src/test/java/org/apache/mesos/elasticsearch/scheduler/FrameworkInfoFactoryTest.java
+++ scheduler/src/test/java/org/apache/mesos/elasticsearch/scheduler/FrameworkInfoFactoryTest.java
@@ -3,6 +3,7 @@ package org.apache.mesos.elasticsearch.scheduler;
 import org.apache.mesos.Protos;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -14,14 +15,18 @@ public class FrameworkInfoFactoryTest {
 
     public static final int DUMMY_PORT = 1234;
 
+    public static final String DUMMY_FRAMEWORK_ROLE = "SomeFrameworkRole";
+
     @Test
     public void shouldGetBuilder() {
         Configuration configuration = mock(Configuration.class);
         when(configuration.getFrameworkName()).thenReturn("TestFrameworkName");
         when(configuration.getWebUiPort()).thenReturn(DUMMY_PORT);
+        when(configuration.getFrameworkRole()).thenReturn(DUMMY_FRAMEWORK_ROLE);
         FrameworkInfoFactory frameworkInfoFactory = new FrameworkInfoFactory(configuration);
         Protos.FrameworkInfo frameworkInfo = frameworkInfoFactory.getBuilder().build();
         assertTrue(frameworkInfo.getWebuiUrl().contains("http://"));
         assertTrue(frameworkInfo.getWebuiUrl().contains(Integer.toString(DUMMY_PORT)));
+        assertEquals(DUMMY_FRAMEWORK_ROLE, frameworkInfo.getRole());
     }
 }
