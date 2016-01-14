fda8e7da3a41a047359640b02fa6e06ed356f1aa
Phil Winder <phil@winderresearch.com>
2016-01-14 10:52:13 +0000

    Reverted to host mode so ES can cluster. LIBPROCESS_IP is now set by hostname, can be overridden.

--- executor/start-executor.sh
+++ executor/start-executor.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-LIBPROCESS_IP=$(ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
+if [ -z "$LIBPROCESS_IP" ]; then
+    # Get the first non local IP address. Make sure you /etc/hosts file is correctly set.
+    export LIBPROCESS_IP=$(hostname -I | awk '{ print $1}' | tr -d "\n")
+fi
+echo "LIBPROCESS_IP is set to '$LIBPROCESS_IP'";
+
 java $JAVA_OPTS -Djava.library.path=/usr/local/lib -jar /tmp/elasticsearch-mesos-executor.jar $@
