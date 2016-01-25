e48460f3c27cc7937d17633c64f6b2b7c8b4e5c7
Phil Winder <phil@winderresearch.com>
2016-01-25 11:41:26 +0000

    Load from URL

--- executor/src/main/java/org/apache/mesos/elasticsearch/executor/Configuration.java
+++ executor/src/main/java/org/apache/mesos/elasticsearch/executor/Configuration.java
@@ -1,12 +1,17 @@
 package org.apache.mesos.elasticsearch.executor;
 
 import com.beust.jcommander.JCommander;
+import org.apache.commons.validator.routines.UrlValidator;
 import org.apache.log4j.Logger;
 import org.apache.mesos.elasticsearch.common.cli.ElasticsearchCLIParameter;
 import org.apache.mesos.elasticsearch.common.cli.HostsCLIParameter;
 import org.apache.mesos.elasticsearch.common.cli.ZookeeperCLIParameter;
 import org.elasticsearch.common.settings.Settings;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
 import java.nio.file.Paths;
 import java.util.List;
 
@@ -17,6 +22,7 @@ public class Configuration {
     private static final Logger LOGGER = Logger.getLogger(Configuration.class);
     public static final String ELASTICSEARCH_YML = "elasticsearch.yml";
     private final ElasticsearchCLIParameter elasticsearchCLI = new ElasticsearchCLIParameter();
+    UrlValidator urlValidator = new UrlValidator();
 
     // **** ZOOKEEPER
     private final ZookeeperCLIParameter zookeeperCLI = new ZookeeperCLIParameter();
@@ -42,7 +48,19 @@ public class Configuration {
         String settingsLocation = getElasticsearchCLI().getElasticsearchSettingsLocation();
         if (settingsLocation.isEmpty()) {
             return Settings.builder();
-        } else {
+        } else if (urlValidator.isValid(settingsLocation)) { // If url
+            final InputStream is;
+            try {
+                final URL url = URI.create(settingsLocation).toURL();
+                LOGGER.debug("Loading settings from url: " + url.toExternalForm());
+                is = url.openStream();
+                return Settings.builder().loadFromStream(settingsLocation, is);
+            } catch (IOException e) {
+                LOGGER.error("Tried to load from URL: " + settingsLocation + ", but failed.", e);
+                throw new IllegalStateException("Unable to load user specified settings file. Please check the URL");
+            }
+        } else { // If file
+            LOGGER.debug("Loading settings from file: " + settingsLocation);
             return Settings.builder().loadFromPath(Paths.get(settingsLocation));
         }
     }
