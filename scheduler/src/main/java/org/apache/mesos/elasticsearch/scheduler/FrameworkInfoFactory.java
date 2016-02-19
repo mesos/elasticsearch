8ac5ed2f6de4a3fbf03aefb25eb303cc6b14361f
Phil Winder <phil@winderresearch.com>
2016-02-19 13:11:01 +0000

    Upgrade dependencies. Speed up build.

--- scheduler/src/main/java/org/apache/mesos/elasticsearch/scheduler/FrameworkInfoFactory.java
+++ scheduler/src/main/java/org/apache/mesos/elasticsearch/scheduler/FrameworkInfoFactory.java
@@ -1,6 +1,6 @@
 package org.apache.mesos.elasticsearch.scheduler;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.apache.mesos.Protos;
 import org.apache.mesos.elasticsearch.scheduler.state.FrameworkState;
